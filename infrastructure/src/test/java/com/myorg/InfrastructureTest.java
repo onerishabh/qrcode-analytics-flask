c0be0268c937c916366d18f1eaca45e621d2ccc2
Rishabh Srivastava <rishabh080598@gmail.com>
2022-08-14 10:41:42 +1000

    clean up

--- infrastructure/src/test/java/com/myorg/InfrastructureTest.java
+++ infrastructure/src/test/java/com/myorg/InfrastructureTest.java
@@ -8,8 +8,6 @@ import java.util.HashMap;
 
 import org.junit.jupiter.api.Test;
 
-// example test. To run these tests, uncomment this file, along with the
-// example resource in java/src/main/java/com/myorg/InfrastructureStack.java
 public class InfrastructureTest {
 
     @Test
